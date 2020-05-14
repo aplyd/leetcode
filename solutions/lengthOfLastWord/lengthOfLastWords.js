f284b4fdd9cc6e8ded4573d8837d853a621ae357
aplyd <austin@ftacnik.com>
2020-05-13 20:53:24 -0700

    incomplete solution but made progress

---
+++ solutions/lengthOfLastWord/lengthOfLastWords.js
@@ -0,0 +1,10 @@
+const lengthOfLastWord = s => {
+	if (s.length > 0) {
+		const arr = s.split(' ')
+		return arr[arr.length - 2].length > 0 ? arr[arr.length - 2].length : 0
+	} else {
+		return 0
+	}
+}
+
+module.exports = lengthOfLastWord
---
+++ solutions/lengthOfLastWord/lengthOfLastWords.test.js
@@ -0,0 +1,31 @@
+const lengthOfLastWord = require('./lengthOfLastWords')
+
+test('', () => {
+	const string = 'hello world'
+	const answer = 5
+	expect(lengthOfLastWord(string)).toBe(answer)
+})
+
+test('', () => {
+	const string = 'a '
+	const answer = 1
+	expect(lengthOfLastWord(string)).toBe(answer)
+})
+
+test('empty string', () => {
+	const string = ' '
+	const answer = 0
+	expect(lengthOfLastWord(string)).toBe(answer)
+})
+
+test('really empty string', () => {
+	const string = '       '
+	const answer = 0
+	expect(lengthOfLastWord(string)).toBe(answer)
+})
+
+test('actual empty string', () => {
+	const string = ''
+	const answer = 0
+	expect(lengthOfLastWord(string)).toBe(answer)
+})

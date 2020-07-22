fcb3956a52e19fa725d5c4992246e93671721add
aplyd <austin@ftacnik.com>
2020-07-21 21:33:54 -0700

    few more solutions

--- solutions/atmPin/atmPin.test.js
+++ solutions/atmPin/atmPin.test.js
@@ -3,7 +3,6 @@ function validatePin(pin) {
 	if (arr.length === 4 || arr.length === 6) {
 		return arr.every(
 			num => num.charCodeAt(0) > 47 && num.charCodeAt(0) < 58
-			// console.log(num.fromCharCodeAt(0))
 		)
 	} else {
 		return false

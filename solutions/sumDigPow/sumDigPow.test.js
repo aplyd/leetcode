e86e30b43a378c18a4c7d1ebca7464999942b87a
aplyd <austin@ftacnik.com>
2020-07-23 15:21:51 -0700

    clean up

--- solutions/sumDigPow/sumDigPow.test.js
+++ solutions/sumDigPow/sumDigPow.test.js
@@ -1,14 +1,16 @@
 function sumDigPow(a, b) {
 	const nums = []
 	const check = num => {
-		const arr = num.toString().split('')
-		const result = arr.reduce((acc, curr, index) => {
-			acc += Math.pow(curr, index + 1)
-			return acc
-		}, 0)
+		const result = num
+			.toString()
+			.split('')
+			.reduce((acc, curr, index) => {
+				acc += Math.pow(curr, index + 1)
+				return acc
+			}, 0)
 		return num === result
 	}
-	console.log(Math.pow(9, 2))
+
 	for (let i = a; i <= b; i++) {
 		if (check(i)) {
 			nums.push(i)

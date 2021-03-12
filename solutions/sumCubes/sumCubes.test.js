63236a0fd7dab31f7a911985aecf21cd9bb9ab0d
aplyd <austin@ftacnik.com>
2021-03-12 11:54:02 -0800

    super easy one

---
+++ solutions/sumCubes/sumCubes.test.js
@@ -0,0 +1,11 @@
+function sumCubes(n) {
+	let total = 0
+	for (let i = 1; i <= n; i++) {
+		total += Math.pow(i, 3)
+	}
+	return total
+}
+
+test('', () => {
+	expect(sumCubes(123)).toBe(58155876)
+})
